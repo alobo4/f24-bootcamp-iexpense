bfab26d0ab555670470abeb29ed8bd178fc8f1f2
alobo4 <lobanov@unc.edu>
2024-10-14 14:18:02 -0400

    iExpense

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # f24-bootcamp-iexpense
+![](https://github.com/alobo4/f24-bootcamp-instagram/blob/main/iExpense.gif)
---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Alec Lobanov on 10/14/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
